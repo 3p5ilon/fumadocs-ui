0834f09fc709ca5a3987b3dd8a6769bf228aaf5b
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-05-23 23:19:05 +0530

    Update

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -8,6 +8,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
+  githubUrl: 'https://github.com',
   nav: {
     title: (
       <>
@@ -19,7 +20,7 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        AI4ALL
       </>
     ),
   },
